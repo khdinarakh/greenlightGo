543bc74cf72d0c380b872fa04d2069b60da3e8e8
khdinarakh <91341711+khdinarakh@users.noreply.github.com>
2022-12-22 22:46:08 +0600

    Add files via upload

---
+++ greenlight/cmd/api/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	print("HERE")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.Parse()
+	print(cfg.db.dsn)
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	print("sadsadasd")
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", cfg.port),
+		Handler:           app.routes(),
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err = srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	// Set the maximum number of open (in-use + idle) connections in the pool. Note that
+	// passing a value less than or equal to 0 will mean there is no limit.
+	db.Config().MaxConns = int32(cfg.db.maxOpenConns)
+	// Set the maximum number of idle connections in the pool. Again, passing a value
+	// less than or equal to 0 will mean there is no limit.
+
+	// Use the time.ParseDuration() function to convert the idle timeout duration string
+	// to a time.Duration type.
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	// Set the maximum idle timeout.
+	db.Config().MaxConnIdleTime = duration
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Success connection")
+
+	return db, nil
+}
---
+++ greenlight/migrations/000003_change_type_version.up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE movies ADD COLUMN new_version UUID NULL;
+UPDATE movies SET new_version = CAST(LPAD(TO_HEX(version), 32, '0') AS UUID);
+ALTER TABLE movies DROP COLUMN version;
+ALTER TABLE movies RENAME COLUMN new_version TO version;
+ALTER TABLE movies ALTER COLUMN version SET NOT NULL;
+
+Select CAST(LPAD(TO_HEX(5), 32, '0');
